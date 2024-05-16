23e04749afa325db10d0224596188151c69c754d
DAVIDDO12 <168615558+DAVIDDO12@users.noreply.github.com>
2024-05-16 18:49:41 +1200

    Update Subroutines.py

--- Subroutines.py
+++ Subroutines.py
@@ -1,1 +1,34 @@
+def triangleTest(side1,side2,side3):
+    triangle = side1 < side2 < side3
+    if triangle and side1 + side2 > side3:
+        return True
+    else:
+        return False
+
+name = input("What's your name:")
+morning = int(input("What's the time(24 hour time)"))
+def time1(morning):
+    if morning < 1200:
+        print("Good morning", name)
+        return True
+    else:
+        return False
+time1(morning)
+afternoon = int(input("What's the time(24 hour time)"))
+def time2(afternoon):
+    if afternoon < 1700:
+        print("Good afternoon", name)
+        return True
+    else:
+        return False
+time2(afternoon)
+evening = int(input("What's the time(24 hour time)"))
+def time3(evening):
+    if evening < 2300:
+        print("Good evening", name)
+        return True
+    else:
+        return False
+time3(evening)
+
 
